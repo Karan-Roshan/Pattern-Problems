09d17807bbabe8c169be01154c1ef3347a41e9e0
Karan Roshan <karanroshan91@gmail.com>
2026-04-26 13:49:02 +0530

    Update patterns Problem

--- Pattern/Symbol Patterns/FullPyramid.java
+++ Pattern/Symbol Patterns/FullPyramid.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
-public class LeftHalfPyramid{
-    public static void main(String arg[]) {
+public class FullPyramid {
+    public static void main(String srg[]) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter n: ");
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<=n ;i++) {
             for(int j=n-i; j>0; j--) {
                 System.out.print(" ");
             }
 
             for(int j=1; j<=i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
 
             System.out.println();
--- Pattern/Symbol Patterns/InvertedLeftHalfPyramid.java
+++ Pattern/Symbol Patterns/InvertedLeftHalfPyramid.java
@@ -1,22 +1,23 @@
 import java.util.*;
 
-public class LeftHalfPyramid{
+public class InvertedLeftHalfPyramid {
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter n: ");
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++) {
-            for(int j=n-i; j>0; j--) {
-                System.out.print(" ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("  ");
             }
 
-            for(int j=1; j<=i; j++) {
-                System.out.print("*");
+            for(int j=1; j<n-i+2; j++) {
+                System.out.print("* ");
             }
 
             System.out.println();
         }
+
     }
 }
