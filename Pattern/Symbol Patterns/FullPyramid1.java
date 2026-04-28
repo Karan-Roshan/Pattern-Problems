9a4268d80bf485400c7cfb5509dc85c48266c1db
Karan Roshan <karanroshan91@gmail.com>
2026-04-28 19:28:35 +0530

    Update patterns & images

---
+++ Pattern/Symbol Patterns/FullPyramid1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FullPyramid1 {
+    public static void main(String srg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n ;i++) {
+            for(int j=n-i; j>0; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
--- Pattern/Symbol Patterns/InvertedFullPyramid2.java
+++ Pattern/Symbol Patterns/InvertedFullPyramid2.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
-public class FullPyramid {
-    public static void main(String srg[]) {
+public class InvertedFullPyramid2 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter n: ");
         int n = sc.nextInt();
 
-        for(int i=1; i<=n ;i++) {
-            for(int j=n-i; j>0; j--) {
-                System.out.print(" ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("  ");
             }
 
-            for(int j=1; j<=i; j++) {
+            for (int j = 1; j <= (n-i) * 2 + 1; j++) {
                 System.out.print("* ");
             }
 
